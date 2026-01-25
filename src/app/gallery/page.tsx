86e44ee384567b441625daa576c1f567f4a97c4b
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2026-01-25 20:09:42 +0000

    Styling gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,17 +1,20 @@
 import Chapter from "@/components/chapterCard";
 import PageWithTitleLayout from "@/components/pageWithTitleLayout";
 import { chapterList } from "@/utils/chapters";
+import styles from "./index.module.scss"
 
 export default function Gallery() {
   let i = 0;
 
   return (
     <PageWithTitleLayout title="Proof Gallery">
-      {
-        chapterList.map(c => 
-          <Chapter key={i++} chapter={c}/>
-        )
-      }
+      <div className={styles.list}>
+        {
+          chapterList.map(c => 
+            <Chapter key={i++} chapter={c}/>
+          )
+        }
+      </div>
     </PageWithTitleLayout>
   );
 }
--- src/components/chapterCard/index.tsx
+++ src/components/chapterCard/index.tsx
@@ -1,4 +1,5 @@
 import { Chapter } from "@/utils/chapters"
+import styles from "./index.module.scss"
 
 type Props = {
     chapter: Chapter
@@ -6,7 +7,7 @@ type Props = {
 
 export default function ChapterCard({ chapter }: Props) {
     return(
-        <div>
+        <div className={styles.container}>
             <p>{chapter.name}</p>
         </div>
     )
