7296ea2cf4234bf495115e9004898cf1c4d4505c
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2026-02-16 18:57:03 +0000

    Removed CameleerBook error page

--- src/app/CameleerBook/error.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { HomePage } from "@/utils/pages";
-import { redirect } from "next/navigation";
-
-export default function CameleerBookHome() {
-    redirect(HomePage.address)
-}
