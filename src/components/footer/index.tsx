404d977d3c7b26714721bb4fe4b0209ad8a11151
PedroGasparinho <p.gasparinho@campus.fct.unl.pt>
2026-02-16 18:39:21 +0000

    Changed footer text order

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,9 +4,9 @@ import styles from "./index.module.scss"
 export default function Footer() {
     return(
         <div className={styles.container}>
-            <BodyText1Alt>Draft Version: 27 Jan. 2026</BodyText1Alt>
             <BodyText1Alt>Pedro Gasparinho, Mário Pereira</BodyText1Alt>
             <BodyText1Alt>{"Contact: p.gasparinho { at } fct.unl.pt"}</BodyText1Alt>
+            <BodyText1Alt>Draft Version: 27 Jan. 2026</BodyText1Alt>
         </div>
     )
 }
